d90263d9068edfc3bdc67e9eb72908aa96682d37
Derek Wang <derek.wang@airbnb.com>
2019-02-21 17:22:57 -0800

    Adds some base Slack publishers for the community

---
+++ publishers/community/slack/slack_layout.py
@@ -0,0 +1,302 @@
+import cgi
+import json
+import time
+import urllib
+from copy import deepcopy
+
+from publishers.core import AlertPublisher, Register
+from stream_alert.shared.description import RuleDescriptionParser
+
+RAUSCH = '#ff5a5f'
+BABU = '#00d1c1'
+LIMA = '#8ce071'
+HACKBERRY = '#7b0051'
+
+@Register
+class PrettyLayout(AlertPublisher):
+    _GITHUB_REPO_URL = 'https://github.com/airbnb/streamalert'
+    _SEARCH_PATH = '/search'
+    _RULES_PATH = '/rules'
+
+    def publish(self, alert, publication):
+        rule_name = alert.rule_name
+        rule_description = alert.rule_description
+        rule_presentation = RuleDescriptionParser.present(rule_description)
+
+        author = rule_presentation['author']
+
+        return {
+            'slack.text': 'Rule triggered',
+            'slack.attachments': [
+                {
+                    'fallback': 'Rule triggered: {}'.format(rule_name),
+                    'color': RAUSCH,
+                    'author_name': author,
+                    'author_link': self._author_url(author),
+                    'author_icon': self._author_icon(author),
+                    'title': rule_name,
+                    'title_link': self._title_url(rule_name),
+                    'text': cgi.escape(rule_presentation['description']),
+                    'fields': map(
+                        lambda(key): {'title': key, 'value': rule_presentation['fields'][key]},
+                        rule_presentation['fields'].keys()
+                    ),
+                    'image_url': '',
+                    'thumb_url': '',
+                    'footer': '',
+                    'footer_icon': '',
+                    'ts': time.mktime(alert.created.timetuple()) if alert.created else '',
+                    'mrkdwn_in': [],
+                },
+            ],
+
+            # This information is passed-through to future publishers.
+            '_previous_publication': publication,
+        }
+
+    @classmethod
+    def _author_url(cls, author):
+        return ''
+
+    @classmethod
+    def _author_icon(cls, author):
+        return ''
+
+    @classmethod
+    def _title_url(cls, rule_name):
+        # It's actually super hard to generate a exact link to a file just from the rule_name,
+        # because the rule/ directory files are not deployed with the publishers in the alert
+        # processor.
+        # Instead, we send them to Github with a formatted query string that is LIKELY to
+        # find the correct file.
+        #
+        # If you do not want URLs to show up, simply override this method and return empty string.
+        return '{}{}?{}'.format(
+            cls._GITHUB_REPO_URL,
+            cls._SEARCH_PATH,
+            urllib.urlencode({
+                'q': '{} path:{}'.format(rule_name, cls._RULES_PATH)
+            })
+        )
+
+
+@Register
+class AttachRuleInfo(AlertPublisher):
+    """This publisher adds a slack attachment with fields from the rule's description
+
+    It can include such fields as "reference" or "playbook" but will NOT include the description
+    or the author.
+    """
+
+    def publish(self, alert, publication):
+        new_publication = deepcopy(publication)
+        new_publication['slack.attachments'] = new_publication.get('slack.attachments', [])
+
+        rule_description = alert.rule_description
+        rule_presentation = RuleDescriptionParser.present(rule_description)
+
+        new_publication['slack.attachments'].append({
+            'color': LIMA,
+            'fields': map(
+                lambda (key): {'title': key, 'value': rule_presentation['fields'][key]},
+                rule_presentation['fields'].keys()
+            )
+        })
+
+        return new_publication
+
+
+@Register
+class AttachPublication(AlertPublisher):
+    """A publisher run after PrettyLayout that attaches previous publications as an attachment"""
+
+    def publish(self, alert, publication):
+        if '_previous_publication' not in publication or 'slack.attachments' not in publication:
+            # This publisher cannot be run except immediately after PrettyLayout
+            return publication
+
+        new_publication = deepcopy(publication)
+
+        publication_block = '```\n{}\n```'.format(
+            json.dumps(
+                publication['_previous_publication'],
+                indent=2,
+                sort_keys=True,
+                separators=(',', ': ')
+            )
+        )
+
+        new_publication['slack.attachments'].append({
+            'color': BABU,
+            'title': 'Alert Data:',
+            'text': cgi.escape(publication_block),
+            'mrkdwn_in': ['text'],
+        })
+
+        return new_publication
+
+
+@Register
+class AttachFullRecord(AlertPublisher):
+    """This publisher attaches slack attachments generated from the Alert's full record
+
+    The full record is likely to be significantly longer than the slack max messages size.
+    So we cut up the record by rows and send it as a series of 1 or more attachments.
+    The attachments are rendered in slack in a way such that a mouse drag and copy will
+    copy the entire JSON in-tact.
+
+    The first attachment is slightly different as it includes the source entity where the
+    record originated from. The last attachment includes a footer.
+    """
+    _SLACK_MAXIMUM_ATTACHMENT_CHARACTER_LENGTH = 4000
+
+    # Reserve space at the beginning and end of the attachment text for backticks and newlines
+    _LENGTH_PADDING = 10
+
+    def publish(self, alert, publication):
+        new_publication = deepcopy(publication)
+        new_publication['slack.attachments'] = new_publication.get('slack.attachments', [])
+
+        # Generate the record and then dice it up into parts
+        record_document = json.dumps(alert.record, indent=2, sort_keys=True, separators=(',', ': '))
+
+        # Escape the document FIRST because it can increase character length which can throw off
+        # document slicing
+        record_document = cgi.escape(record_document)
+        record_document_lines = record_document.split('\n')
+
+        def make_attachment(document, is_first, is_last):
+
+            footer = ''
+            if is_last:
+                footer_url = self._source_service_url(alert.source_service)
+                if footer_url:
+                    footer = 'via <{}|{}>'.format(footer_url, alert.source_service)
+                else:
+                    'via {}'.format(alert.source_service)
+
+            return {
+                'color': HACKBERRY,
+                'author': alert.source_entity if is_first else '',
+                'title': 'Record' if is_first else '',
+                'text': '```\n{}\n```'.format(document),
+                'fields': [
+                    {
+                        "title": "Alert Id",
+                        "value": alert.alert_id,
+                    }
+                ] if is_last else [],
+                'footer': footer,
+                'footer_icon': '',
+                'mrkdwn_in': ['text'],
+            }
+
+        character_limit = self._SLACK_MAXIMUM_ATTACHMENT_CHARACTER_LENGTH - self._LENGTH_PADDING
+        is_first_document = True
+        next_document = ''
+        while len(record_document_lines) > 0:
+            # Loop, removing one line at a time and attempting to attach it to the next document
+            # When the next document nears the maximum attachment size, it is flushed, generating
+            # a new attachment, and the document is reset before the loop pops off the next line.
+
+            next_item_length = len(record_document_lines[0])
+            next_length = next_item_length + len(next_document)
+            if next_document and next_length > character_limit:
+                # Do not pop off the item just yet.
+                new_publication['slack.attachments'].append(
+                    make_attachment(next_document, is_first_document, False)
+                )
+                next_document = ''
+                is_first_document = False
+
+            next_document += '\n' + record_document_lines.pop(0)
+
+        # Attach last document, if any remains
+        if next_document:
+            new_publication['slack.attachments'].append(
+                make_attachment(next_document, is_first_document, True)
+            )
+
+        return new_publication
+
+    @staticmethod
+    def _source_service_url(source_service):
+        return 'https://console.aws.amazon.com/{}/home'.format(source_service)
+
+
+# Sample
+'''
+{
+	"text": "Rule triggered",
+    "attachments": [
+        {
+            "fallback": "Rule triggered: multi_sudo_wget_curl_generic",
+            "color": "#ff5a5f",
+            "author_name": "javier_marcos",
+            "author_link": "",
+            "author_icon": "",
+            "title": "multi_sudo_wget_curl_generic",
+            "title_link": "https://git.musta.ch/csirt/streamalert/search?q=multi_sudo_wget_curl_generic+path%3A%2Frules",
+            "text": "Identify use of wget/curl commands that utilize sudo.",
+            "footer": "via Rules",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 1550386041
+        },
+        {
+            "color": "#8ce071",
+             "fields": [
+				{
+                    "title": "playbook",
+                    "value": "(a) look at the fetched resource (malicious?) (b) look at the user (expected?)"
+                },
+                {
+                    "title": "att&ck tactic",
+                    "value": "Execution"
+                },
+                {
+                    "title": "att&ck technique",
+                    "value": "Command-line Interface"
+                },
+                {
+                    "title": "att&ck url",
+                    "value": "https://attack.mitre.org/wiki/Technique/T1059"
+                }
+            ]
+        },
+        {
+			"color": "#00d1c1",
+			"author_name": "",
+			"title": "Alert Data:",
+			"text": "\n```\n{\n  'field1': 'value',\n  'field2': 'value',\n  'field3': {\n    'subfield1': 'v',\n    'subfield2': 'z',\n    'subfield3': 'q',\n```",
+			"fields": [],
+			"mrkdwn_in": ["text"]
+		},
+		{
+			"color": "#7b0051",
+			"author_name": "airbnb.s3.carbonblack.us-east-1",
+			"title": "Record:",
+			"text": "\n```\n{\n  'AlertCount': '1',\n  'AlertTimeFirst': '2019-02-17T06:47:33.475007Z',\n  'AlertTimeLast': '2019-02-17T06:47:33:470557Z',\n  'MergedBy': {\n    'cmdline': 'sudo -u rails RAILS_ENV=production sciprt/silhouette.rb /srv/silhouette.yml b2e7b434-749a-4e71-aef2-943d598aea35 run_requests_from_ruL 2 curl',\n```",
+			"fields": [],
+			"mrkdwn_in": ["text"]
+		},
+		{
+			"color": "#7b0051",
+			"text": "\n```\n    'cwd': '/mnt/titanic/monorail/monotail_6908847686533bf440c6a0a72ea071f152a19a3c9_distribution-ubuntu_node_version-v8-9-1',\n    'gid': '1088',\n    'name': 'sudo',\n    'parent': '32750',\n    'path': '/usr/bin/sudo',\n    'pid': '32753',\n```",
+			"mrkdwn_in": ["text"]
+		},
+		{
+			"color": "#7b0051",
+			"text": "```\n    'system_time': '0'\n  },\n  'counter': '80',\n  'decorations': {\n    'envIdentifier': 'prod',\n    'roleIdentifier': 'silhouette-runner',\n  }\n}\n```",
+			"fields": [
+				{
+					"title": "Alert ID",
+					"value": "aha378-38acbae3-0830b04a-04b4084"
+				}
+			],
+			"footer": "via <https://www.google.com|s3>",
+			"footer_icon": "https://cdn-images-1.medium.com/max/1200/1*Bbdx_ZjoHiUfjRaYha8xkQ.png",
+			"mrkdwn_in": ["text"]
+		}
+    ]
+}
+'''
---
+++ tests/unit/stream_alert_alert_processor/test_publishers/slack/test_slack_layout.py
@@ -0,0 +1,195 @@
+from datetime import datetime
+
+from nose.tools import assert_equal, assert_true, assert_less_equal
+
+from publishers.community.slack.slack_layout import PrettyLayout, AttachRuleInfo, AttachPublication, \
+    AttachFullRecord
+from tests.unit.stream_alert_alert_processor.helpers import get_alert
+
+
+class TestPrettyLayout(object):
+
+    def setup(self):
+        self._publisher = PrettyLayout()
+
+    def test_simple(self):
+        """Publishers - Slack - PrettyLayout"""
+        alert = get_alert()
+        alert.created = datetime(2019, 1, 1)
+
+        publication = self._publisher.publish(alert, {})
+
+        expectation = {
+            'slack.text': 'Rule triggered',
+            '_previous_publication': {},
+            'slack.attachments': [
+                {
+                    'author_link': '',
+                    'color': '#ff5a5f',
+                    'text': 'Info about this rule and what actions to take',
+                    'author_name': '',
+                    'footer_icon': '',
+                    'mrkdwn_in': [],
+                    'thumb_url': '',
+                    'title': 'cb_binarystore_file_added',
+                    'fields': [],
+                    'footer': '',
+                    'ts': 1546329600.0,
+                    'title_link': (
+                        'https://github.com/airbnb/streamalert/search'
+                        '?q=cb_binarystore_file_added+path%3A%2Frules'
+                    ),
+                    'image_url': '',
+                    'fallback': 'Rule triggered: cb_binarystore_file_added',
+                    'author_icon': ''
+                }
+            ]
+        }
+
+        assert_equal(publication, expectation)
+
+
+class TestAttachRuleInfo(object):
+
+    def setup(self):
+        self._publisher = AttachRuleInfo()
+
+    def test_simple(self):
+        """Publishers - Slack - AttachRuleInfo"""
+        alert = get_alert()
+        alert.created = datetime(2019, 1, 1)
+        alert.rule_description = '''
+Author: unit_test
+Reference: somewhere_over_the_rainbow
+Description: ?
+Att&ck vector:  Assuming direct control
+'''
+
+        publication = self._publisher.publish(alert, {})
+
+        expectation = {
+            'slack.attachments': [
+                {
+                    'color': '#8ce071',
+                    'fields': [
+                        {
+                            'title': 'att&ck vector',
+                            'value': 'Assuming direct control',
+                        },
+                        {
+                            'title': 'reference',
+                            'value': 'somewhere_over_the_rainbow',
+                        }
+                    ]
+                }
+            ]
+        }
+
+        assert_equal(publication, expectation)
+
+
+class TestAttachPublication(object):
+
+    def setup(self):
+        self._publisher = AttachPublication()
+
+    def test_simple(self):
+        """Publishers - Slack - AttachPublication"""
+        alert = get_alert()
+        alert.created = datetime(2019, 1, 1)
+
+        previous = {
+            '_previous_publication': {'foo': 'bar'},
+            'slack.attachments': [
+                {
+                    'text': 'attachment1',
+                },
+            ]
+        }
+        publication = self._publisher.publish(alert, previous)
+
+        expectation = {
+            '_previous_publication': {'foo': 'bar'},
+            'slack.attachments': [
+                {'text': 'attachment1'},
+                {
+                    'color': '#00d1c1',
+                    'text': '```\n{\n  "foo": "bar"\n}\n```',
+                    'mrkdwn_in': ['text'],
+                    'title': 'Alert Data:'
+                }
+            ]
+        }
+
+        assert_equal(publication, expectation)
+
+
+class TestAttachFullRecord(object):
+
+    def setup(self):
+        self._publisher = AttachFullRecord()
+
+    def test_simple(self):
+        """Publishers - Slack - AttachFullRecord"""
+        alert = get_alert()
+        alert.created = datetime(2019, 1, 1)
+
+        publication = self._publisher.publish(alert, {})
+
+        expectation = {
+            'slack.attachments': [
+                {
+                    'footer': 'via <https://console.aws.amazon.com/s3/home|s3>',
+                    'fields': [
+                        {'value': '79192344-4a6d-4850-8d06-9c3fef1060a4', 'title': 'Alert Id'}
+                    ],
+                    'mrkdwn_in': ['text'],
+                    'author': 'corp-prefix.prod.cb.region',
+                    'color': '#7b0051',
+                    'text': (
+                        '```\n\n{\n  "cb_server": "cbserver",\n  "compressed_size": "9982",'
+                        '\n  "file_path": "/tmp/5DA/AD8/0F9AA55DA3BDE84B35656AD8911A22E1.zip",'
+                        '\n  "md5": "0F9AA55DA3BDE84B35656AD8911A22E1",\n  "node_id": "1",'
+                        '\n  "size": "21504",\n  "timestamp": "1496947381.18",'
+                        '\n  "type": "binarystore.file.added"\n}\n```'
+                    ),
+                    'title': 'Record',
+                    'footer_icon': ''
+                }
+            ]
+        }
+        assert_equal(publication, expectation)
+
+    def test_record_splitting(self):
+        """Publishers - Slack - AttachFullRecord - Split Record"""
+        alert = get_alert()
+        alert.created = datetime(2019, 1, 1)
+
+        alert.record = {
+            'massive_record': []
+        }
+        for index in range(0, 999):
+            alert.record['massive_record'].append({
+                'index': index,
+                'value': 'foo'
+            })
+
+        publication = self._publisher.publish(alert, {})
+
+        attachments = publication['slack.attachments']
+
+        assert_equal(len(attachments), 14)
+        for attachment in attachments:
+            assert_less_equal(len(attachment['text']), 4000)
+
+        assert_equal(attachments[0]['title'], 'Record')
+        assert_equal(len(attachments[0]['fields']), 0)
+        assert_equal(attachments[0]['footer'], '')
+
+        assert_equal(attachments[1]['title'], '')
+        assert_equal(len(attachments[1]['fields']), 0)
+        assert_equal(attachments[1]['footer'], '')
+
+        assert_equal(attachments[13]['title'], '')
+        assert_equal(len(attachments[13]['fields']), 1)
+        assert_equal(attachments[13]['footer'], 'via <https://console.aws.amazon.com/s3/home|s3>')
