5b52a92632c44e2bb174913928c944159c88dd31
Derek Wang <derek.wang@airbnb.com>
2019-02-27 10:45:55 -0800

    fix licenses

--- tests/unit/stream_alert_alert_processor/test_publishers/slack/test_slack_layout.py
+++ tests/unit/stream_alert_alert_processor/test_publishers/slack/test_slack_layout.py
@@ -1,6 +1,22 @@
+"""
+Copyright 2017-present, Airbnb Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+# pylint: disable=protected-access,attribute-defined-outside-init
 from datetime import datetime
 
-from nose.tools import assert_equal, assert_true, assert_less_equal
+from nose.tools import assert_equal, assert_less_equal
 
 from publishers.community.slack.slack_layout import Summary, AttachRuleInfo, AttachPublication, \
     AttachFullRecord
