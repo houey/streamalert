402ac6378039ddfbe161cb73c6f9244c1583626f
Derek Wang <derek.wang@airbnb.com>
2019-02-27 14:44:38 -0800

    Add publishers

--- publishers/community/generic.py
+++ publishers/community/generic.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from stream_alert.shared.publisher import Register, AlertPublisher
+from stream_alert.shared.publisher import Register
 
 
 @Register
@@ -39,3 +39,56 @@ def remove_internal_fields(_, publication):
     publication.pop('outputs', None)
 
     return publication
+
+
+@Register
+def enumerate_fields(_, publication):
+    """Flattens all currently published fields.
+
+    By default, publications are deeply nested dict structures. This can be very hard to read
+    when rendered in certain outputs. PagerDuty is one example where
+
+    This publisher collapses deeply nested fields into a single-leveled dict with keys that
+    correspond to the original path of each value in a deeply nested dict. For example:
+
+    {
+      "top1": {
+        "mid1": "low",
+        "mid2": [ "low1", "low2", "low3" ],
+        "mid3": {
+          "low1": "verylow"
+        }
+      },
+      "top2": "mid"
+    }
+
+    .. would collapse into the following structure:
+
+    {
+      "top1.mid1": "low",
+      "top1.mid2[0]": "low1",
+      "top1.mid2[1]": "low1",
+      "top1.mid2[2]": "low1",
+      "top1.mid3.low1: "verylow",
+      "top2": "mid"
+    }
+    """
+    def enumerate_fields(structure, output, path=''):
+        if isinstance(structure, list):
+            for index, item in enumerate(structure):
+                enumerate_fields(item, '{}[{}]'.format(path, index))
+
+        elif isinstance(structure, dict):
+            for key in structure:
+                enumerate_fields(structure[key], '{prefix}{key}'.format(
+                    prefix='{}.'.format(path) if path else '',  # Omit first period
+                    key=key
+                ))
+
+        else:
+            output[path] = structure
+
+    output = {}
+    enumerate_fields(publication, output)
+
+    return output
